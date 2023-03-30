c01f5306d4f21ae85218c12fdfe61d5bfa6b180c
mani0000007 <124129703+mani0000007@users.noreply.github.com>
2023-03-30 17:57:57 +0530

    updated

--- myFile.py
+++ myFile.py
@@ -57,6 +57,56 @@ CROSS JOIN rates r
 WHERE YEAR(terminationdate_key) = 2020
 GROUP BY r.Rate;
 
+==============================================================================
+D)	What would be the projected terminations if it is expected to grow by 4.5%? 
+================================================================================
 
+%sql
+select count(*)+count(*)*4.5/100 as projected_terminations from btd where YEAR(terminationdate_key) = 2020
+
+
+# =====================================================================================================================
+E)	Create a summary table to show the total termination for each termination reason (Retirement, Resignation, Layoff),       
+•	Which one is the highest? 
+==========================================================================================================================
+
+%sql
+select termreason_desc, count(*) as Total_Terminations from btd group by termreason_desc order by Total_Terminations desc limit 1;
+
+============================================================================================
+F)	Brightness is thinking of introducing a categorization of contract terminations, if the termination was a retirement or a resignation then it is a “Voluntary” termination, if it was a layoff then it is an “Involuntary” termination.
+a.	Based on the above description Fill out the termtype_desc column.	
+b.	What percentage of total terminations were voluntary?		________
+===========================================================================================
+
+%sql
+SELECT (voluntary_count * 100 / total_count) AS Percentage_Voluntary_Terminations
+FROM (
+  SELECT COUNT(*) AS total_count, 
+         COUNT(*) FILTER (WHERE termtype_desc = 'Voluntary') AS voluntary_count
+  FROM btd
+)
+
+
+=====================================================================================================
+G)	You are to study the experience level of terminated employees based on the following rough classification.									
+•	If the employee has served for 5 years or less then he/she is a “Newcomer”
+•	If the employee has served between 6 to 10 years, then he/she is a “Proficient”
+•	If the employee has served for more than 10 years, the he/she is an “Expert’
+
+======================================================================================================
+
+
+
+%sql
+SELECT 
+  CASE 
+    WHEN DATEDIFF(terminationdate_key, hiredate_key)/365 <= 5 THEN 'Newcomer'
+    WHEN DATEDIFF(terminationdate_key, hiredate_key)/365 BETWEEN 6 AND 10 THEN 'Proficient'
+    ELSE 'Expert'
+  END AS Experience_Level,
+  COUNT(*) AS Total_Terminations
+FROM btd
+GROUP BY Experience_Level;
 
 
